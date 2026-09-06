6fd19ac502d7569b450785d94066bcd8494f2af5
yatogami <brook.self667@slmail.me>
2026-09-06 18:53:15 +0530

    y

--- marketplace.js
+++ marketplace.js
@@ -1,3 +1,23 @@
+let items=document.querySelector('.items-flex');
+import mydata from "./shops.json" with {type:"json"};
+mydata.forEach((shop) => {
+shop.items.forEach((item) => {
+let temp = document.createElement("div");
+temp.className = "item-flex";
+items.appendChild(temp);
+temp.innerHTML = `
+              <img class="item-image" src="${shop.src}">
+              <p class="shopname">${shop.name}</p>
+              <h3 class="item-name">${item.name}</h3>
+              <p>${item.desc}</p>
+              <p>Price: ₹${item.price}</p>
+              <p>Open Days: ${shop.opendays}</p>
+              <p>Open hours: ${shop.openhours}</p>
+              <p>Contacts: ${shop.contact}</p>
+`;
+});
+});
+
 function changeNavBar() {
     var x = document.getElementById("main-nav");
     if (x.className == "main-nav") {
@@ -30,22 +50,3 @@ searchInput.addEventListener("input", function () {
     }
 });
 
-let items=document.querySelector('.items-flex');
-import { mydata } from "./shops.json" with {type:"json"};
-mydata.forEach((shop) => {
-shop.items.forEach((item) => {
-let temp = document.createElement("div");
-temp.className = "item-flex";
-items.appendChild(temp);
-temp.innerHTML = `
-              <img class="item-image" src="${shop.src}">
-              <p class="shopname">${shop.name}</p>
-              <h3 class="item-name">${item.name}</h3>
-              <p>${item.desc}</p>
-              <p>Price: ₹${item.price}</p>
-              <p>Open Days: ${shop.opendays}</p>
-              <p>Open hours: ${shop.openhours}</p>
-              <p>Contacts: ${shop.contact}</p>
-`;
-});
-});
