8b783fda245593d835e9c7c75da4384623cc147c
yatogami <brook.self667@slmail.me>
2026-09-06 18:28:34 +0530

    y

---
+++ userdat.js
@@ -0,0 +1,19 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://jxasieuljeucnbmlneve.supabase.co', 'sb_publishable_YYOWZb4RZ3u3gurJd0gqmg_s7GKRE0y');
+
+const options = {
+  db: {
+    schema: 'public',
+  },
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true
+  },
+  global: {
+    headers: { 'x-my-custom-header': 'my-app-name' },
+  },
+}
+const supabase = createClient("https://xyzcompany.supabase.co", "your-publishable-key", options)
